9ace422056a3cdf071a00b05c73370ced82f3fe7
politinizer <113183482+likemindsforpeterobi@users.noreply.github.com>
2023-05-05 08:54:23 +0100

    Search bar added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FetchopenskyapiService } from '../fetchopenskyapi.service';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   //
   flights: any[] = [];
   isLoading: boolean = true;
@@ -18,20 +18,26 @@ export class DashboardComponent {
     private openSky: FetchopenskyapiService,
     private auth: AuthService,
     private router: Router
-  ) {
+  ) {}
+
+  ngOnInit() {
     const begin = 1517227200;
     const end = 1517230800;
 
-    this.openSky.getFlight(begin, end).subscribe(
-      (data) => {
+    this.openSky.getFlight(begin, end).subscribe({
+      next: (data) => {
         this.isLoading = false;
+        // console.log('data from dashboard.ts: ', data);
         this.flights = data;
+        console.log('flight data from dashboard.ts: ', this.flights);
       },
-      (error) => {
+      error: (error) => {
+        console.log(error);
         this.isLoading = false;
         this.error = error.message;
-      }
-    );
+      },
+      complete: () => console.log('call to api complete successfully'),
+    });
   }
 
   logout() {
--- src/app/flight-table/flight-table.component.ts
+++ src/app/flight-table/flight-table.component.ts
@@ -1,12 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'flight-table',
   templateUrl: './flight-table.component.html',
   styleUrls: ['./flight-table.component.css'],
 })
-export class FlightTableComponent {
+export class FlightTableComponent implements OnChanges {
   @Input() flights_info: any[] = [];
   @Input() isLoading: boolean = true;
   @Input() error: string = '';
+
+  searchQuery: string = '';
+  filteredFlights: any[] = [];
+
+  constructor() {}
+
+  ngOnChanges() {
+    if (this.flights_info) {
+      this.filteredFlights = this.flights_info.filter((flight) => {
+        return (
+          flight.callsign &&
+          flight.callsign.toLowerCase().includes(this.searchQuery.toLowerCase())
+        );
+      });
+    }
+  }
+
+  search(query: string) {
+    console.log(query);
+    this.filteredFlights = query
+      ? this.flights_info.filter((f) =>
+          f.callsign?.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.flights_info;
+  }
 }
